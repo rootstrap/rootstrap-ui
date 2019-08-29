25dbe3a54fa24e5c46902548c9f6c8b95f910cfc
Bernabé González <bernabe@rootstrap.com>
2019-08-29 19:36:24 -0300

    storybook setup (#1)

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        loader: 'file-loader?name=[name].[ext]'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+        include: path.resolve(__dirname, '../')
+      },
+      {
+        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+    ]
+  }
+};
---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+
+module.exports = ctx => ({
+  syntax: 'postcss-scss',
+  plugins: ctx.env === 'development'
+    ? [
+      autoprefixer({ grid: true }),
+    ]
+    : [
+      autoprefixer({ grid: true }),
+      cssnano({ preset: 'default' }),
+    ],
+})
